1c7c822e5552b8ce620a18d508de261231d32c59
beppek <beppe@live.se>
2017-02-19 20:26:52 +0100

    fix name conflict

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,23 @@ const firebase = new FirebaseInterface();
 
 function users(server) {
 
-    server.get({name: "users", path: "/users"}, (req, res, next) => {
+    server.get({path: "/users"}, (req, res, next) => {
         res.header("Allow", "POST");
-        return res.send(405);
+        res.send(405);
+        return next();
     });
 
-    server.post({name: "users", path: "/users"}, (req, res, next) => {
+    server.post({path: "/users"}, (req, res, next) => {
         firebase.createUser(req.body)
             .then((user) => {
                 console.log(user);
-                return res.send(200, {displayName: user.displayName, uid: user.uid});
+                res.send(200, {displayName: user.displayName, uid: user.uid});
+                return next();
             })
             .catch((error) => {
                 console.log(error);
-                return res.send(error.code, {error: error.message});
+                res.send(error.code, {error: error.message});
+                return next();
             });
     });
 
